e2d4bb813b75e32c600a46e44ca7fb9cd2e9be4e
Aleksey Koshkin <ekbdizzy@yandex.ru>
2022-07-14 18:40:14 +0600

    Customize order model

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Order
 
-# Register your models here.
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        "number",
+        "order_number",
+        "dollar_price",
+        "rubble_price",
+        "delivery_date",
+    )
+
+    list_filter = ("delivery_date",)
+    search_fields = (
+        "number",
+        "order_number",
+        "dollar_price",
+        "rubble_price",
+    )
--- orders/models.py
+++ orders/models.py
@@ -6,16 +6,25 @@ class Order(models.Model):
     number = models.PositiveIntegerField('№', unique=True, db_index=True)
     order_number = models.PositiveIntegerField('Заказ №', unique=True, db_index=True)
     dollar_price = models.DecimalField(
-        'Стоимость,$',
+        'Стоимость, $',
         decimal_places=0,
         max_digits=9,
         validators=[MinValueValidator(1)],
+        db_index=True,
     )
     rubble_price = models.DecimalField(
         'Стоимость, руб.',
         decimal_places=2,
         max_digits=11,
         validators=[MinValueValidator(1)],
+        db_index=True,
     )
     delivery_date = models.DateField('Дата поставки')
 
+    def __str__(self):
+        return f'Заказ № {self.order_number}, стоимость: {self.dollar_price}$'
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+        ordering = ('delivery_date',)
